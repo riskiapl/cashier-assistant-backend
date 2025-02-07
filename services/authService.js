af4774a6636b6a7ae6580309471a01500b8cb739
Riski Agung <riski97@gmail.com>
2025-02-07 16:29:16 +0700

    update register validation]

--- services/authService.js
+++ services/authService.js
@@ -12,24 +12,21 @@ async function registerMember(username, email, password) {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Check if email is already registered
-  const existingEmail = await members.findOne({
-    where: { email, action_type: { [Op.ne]: "D" } },
+  // Check if email or username is already registered
+  const existingUser = await members.findOne({
+    where: {
+      [Op.or]: [{ email }, { username }],
+    },
   });
 
-  if (existingEmail) {
-    throw new Error("Email is already registered");
-  }
-
-  // Check if username is already registered
-  const existingUsername = await members.findOne({
-    where: { username, action_type: { [Op.ne]: "D" } },
-  });
-
-  if (existingUsername) {
-    throw new Error(
-      "Username is already registered, please use another username"
-    );
+  if (existingUser) {
+    if (existingUser.email === email) {
+      throw new Error("Email is already registered");
+    } else {
+      throw new Error(
+        "Username is already taken, please choose another username"
+      );
+    }
   }
 
   const existingPendingUsername = await pending_members.findOne({
